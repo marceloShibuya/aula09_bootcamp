a7711d88570e3b1b353414a3ab8fb88cc75ac8fb
Marcelo Shibuya <marcelo.hshibuya@gmail.com>
2024-08-06 20:34:16 -0300

    adicionando exemplos para salvar e criar arquivos de logs, e tipos de logs

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,16 +1,36 @@
 from loguru import logger
 
+# def soma(x,y):
+#     # print(x)
+#     logger.info(x)
+#     # print(y)
+#     logger.info(y)
+#     # print(x + y)
+#     logger.info(x + y)
+#     return x + y
+
+# total = soma(1,2)
+# print(total)
+
+"""
+O logger.add() é uma função que cria o arquivo .log
+"""
+logger.add("meu_log.log")
+
+"""
+Salvando apenas os logs que são críticos
+"""
+logger.add("meu_log_critico.log", level="CRITICAL")
+
 def soma(x,y):
-    # print(x)
-    logger.info(x)
-    # print(y)
-    logger.info(y)
-    # print(x + y)
-    logger.info(x + y)
-    return x + y
-
-total = soma(1,2)
-print(total)
+    try:
+        soma = x + y
+        logger.info(f"Você digitou corretamente e a soma dos números é: {soma}")
+        return soma
+    except:
+        logger.critical("É preciso digitar valores corretos")
 
 
-
+soma(1,4)
+soma(2,2)
+soma(2,"3")
