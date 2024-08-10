467d274c8b3474b908ae99931f6561deaa7e0ceb
Marcelo Shibuya <marcelo.hshibuya@gmail.com>
2024-08-10 16:21:44 -0300

    utilizando o decorador da funcao criada no arquivo utils_log.py

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,18 +1,33 @@
 from loguru import logger
+from utils_log import log_decorator
 
-logger.add(
-            "logs_critical.log",
-            format = "{time} {level} {message} {file}",
-            level = "CRITICAL"
-)
+# logger.add(
+#             "logs_critical.log",
+#             format = "{time} {level} {message} {file}",
+#             level = "CRITICAL"
+# )
 
+# def soma(x,y):
+#     try:
+#         total = x + y
+#         logger.info(f"O total da soma é de: {total}")
+#         return total
+#     except:
+#         logger.critical("Por favor, digite números inteiros")
+
+
+# soma(1,10)
+# soma(20,1)
+# soma(1,"2")
+
+"""
+Utilizando o decorador
+"""
+
+@log_decorator
 def soma(x,y):
-    try:
-        total = x + y
-        logger.info(f"O total da soma é de: {total}")
-        return total
-    except:
-        logger.critical("Por favor, digite números inteiros")
+    total = x + y
+    return total
 
 
 soma(1,10)
